a011d5c13c2921c254abdd9c156b5491299799e1
unknown <mauriciorasouza@gmail.com>
2024-06-11 19:56:30 -0300

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AppNavigator from './src/utils/Navegacao';
+import AppNavigator from './src/components/Navegacao';
 
 export default function App() {
   return <AppNavigator />;
